609ba213bf93475c689cf31d8f43211d6d97c58f
boyboy0710 <boyboy0710@naver.com>
2021-04-24 12:50:53 +0900

    c

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("안녕")
+name = input("이름입력=")
+a = int(input("점수입력="))
+
+if a >= 90:
+    hakjum = "A"
+
+elif a>= 80:
+    hakjum = "B"
+
+elif a>= 70:
+    hakjum = "C"
+
+elif a>= 60:
+    hakjum = "D"
+
+else:
+    hakjum = "F"
+
+print(name,"님의 학점은",hakjum,"입니다")
